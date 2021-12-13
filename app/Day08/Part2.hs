34b5c95f4bc93bb45730a4c1335d5b2cfc880488
Paul Bedaride <pbedaride@quarkslab.com>
2021-12-13 12:02:45 +0100

    Day 08

---
+++ app/Day08/Part2.hs
@@ -0,0 +1,55 @@
+
+module Main where
+
+import qualified Data.Set as Set
+import Data.List (elemIndex)
+import Data.List.Split (splitOn)
+import Data.Maybe (fromMaybe)
+import Text.Printf (printf)
+import Util (inputRaw)
+
+
+-- | Read the input file.
+input :: String -> [([Set.Set Char], [Set.Set Char])]
+input fileName = map parseLine $ lines $ inputRaw fileName
+
+parseLine :: String -> ([Set.Set Char], [Set.Set Char])
+parseLine line = (signal_patterns, digits_output)
+    where
+        [signal_patterns_, digits_output_] = splitOn " | " line
+        signal_patterns = map Set.fromList $ splitOn " " signal_patterns_
+        digits_output = map Set.fromList $ splitOn " " digits_output_
+
+
+process :: [([Set.Set Char], [Set.Set Char])] -> Int
+process ((signal_pattern, digits_outputs):other) =
+    (subProcess signal_pattern digits_outputs) + process other
+process [] = 0
+
+subProcess :: [Set.Set Char] -> [Set.Set Char] -> Int
+subProcess signal_patterns digits_outputs = 
+    foldl (\x y -> x * 10 + y) 0 $ map (\x -> fromMaybe 0 $ elemIndex x patterns) digits_outputs
+    where
+        one = head $ filter (\x -> Set.size x == 2) signal_patterns
+        seven = head $ filter (\x -> Set.size x == 3) signal_patterns
+        four = head $ filter (\x -> Set.size x == 4) signal_patterns
+        eight = head $ filter (\x -> Set.size x == 7) signal_patterns
+        nine = head $ filter (\x -> Set.size x == 6 && (Set.union four seven) `Set.isSubsetOf` x) signal_patterns
+        five = head $ filter (\x -> Set.size x == 5 && (Set.difference nine seven) `Set.isSubsetOf` x) signal_patterns
+        three = head $ filter (\x -> Set.size x == 5 && seven `Set.isSubsetOf` x) signal_patterns
+        two = head $ filter (\x -> Set.size x == 5 && (Set.difference eight four) `Set.isSubsetOf` x) signal_patterns
+        zero = head $ filter (\x -> Set.size x == 6 && (Set.difference eight five) `Set.isSubsetOf` x) signal_patterns
+        six = head $ filter (\x -> Set.size x == 6 && (Set.difference two seven) `Set.isSubsetOf` x) signal_patterns
+        patterns = [ zero
+                   , one
+                   , two
+                   , three
+                   , four
+                   , five
+                   , six
+                   , seven
+                   , eight
+                   , nine]
+
+main :: IO ()
+main = printf "Day08 Part2: %s" $ show $ process $ input "inputs/08"
