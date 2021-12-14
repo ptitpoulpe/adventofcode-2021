0e13e4d12164cdfc15a1619b1c6159afcfc66350
Paul Bedaride <pbedaride@quarkslab.com>
2021-12-14 09:12:27 +0100

    Add debug functions to Util

--- src/Util.hs
+++ src/Util.hs
@@ -1,9 +1,17 @@
 -- |
 -- Various utility functions.
 module Util where
-
+import qualified Data.Text.Lazy as LT
+import Debug.Trace (trace)
+import Text.Pretty.Simple (pShow)
 import System.IO.Unsafe (unsafePerformIO)
 
 -- | get the (raw) input (as one line (with newlines in it))
 inputRaw :: String -> String
 inputRaw fileName = unsafePerformIO $ readFile fileName
+
+pTraceShow :: Show a => a -> b -> b
+pTraceShow x = trace (LT.unpack (pShow x))
+
+pTraceShowId :: Show a => a -> a
+pTraceShowId x = pTraceShow x x
